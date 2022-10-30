b9988902d338889abe8f8981e02169ab440266d8
USER <>
2022-10-30 22:24:23 +0900

    no message

---
+++ Gato/ContentView.swift
@@ -0,0 +1,174 @@
+//
+//  ContentView.swift
+//  Gato
+//
+//  Created by Ai on 2022/10/21.
+//
+import SwiftUI
+
+struct ContentView : View {
+    @State var zoomed = false
+    
+    var body: some View {
+        
+        VStack(spacing: 120) {
+            ZStack {
+                LinearGradient(
+                    colors: [Color("Color-1"), Color("Color")],
+                    startPoint: .trailing,
+                                endPoint: .center
+                            )
+                
+                .cornerRadius(18)
+                .ignoresSafeArea()
+                
+                //alingment change to the left after...
+                VStack(alignment: .center) {
+                   
+                    
+                    HStack(alignment: .center, spacing: 10) {
+                        Text("Do you want to play with fluffy cats while enjoying drinking delicious and cute drinks?")
+                            .font(.title3)
+                            .fontWeight(.semibold)
+                            .foregroundColor(.white)
+                            .padding(10)
+
+                        .lineLimit(5)
+                        .fixedSize(horizontal: false, vertical: true)
+                        .shadow(radius: 10)
+                        
+
+
+                    }
+                    
+                    GeometryReader { geometry in
+                        VStack(spacing: 5) {
+                        Image(systemName: "pawprint")
+                            .font(.system(size: 40 ))
+                            .frame(width: 0)
+                    
+                        Image(systemName: "pawprint")
+                        .font(.system(size: 30 ))
+                        .frame(width: 50, height: 40 )
+                        Image(systemName: "pawprint")
+                            .font(.system(size :  30 ))
+                            .frame(width :  60, height: 40 )
+                        Image(systemName: "pawprint")
+                            .font(.system(size : 30 ))
+                            .frame(width :  50, height: 50)
+                            
+                        
+                    }
+                    }
+                    HStack(alignment: .center) {
+                        Image("kono")
+                            .clipShape(Circle())
+                        .shadow(radius: 15)
+                        .overlay(Circle().stroke(Color.white, lineWidth: 5))
+                    
+                    }
+                }
+            }
+          
+            //delated a zstack
+        
+
+            VStack {
+                    
+                   /* Text("Moff Cafe")
+                    
+                        .font(.title)
+                        .fontWeight(.medium)
+                        .shadow(radius: 5)
+                        .foregroundColor(.teal)*/
+                    
+                    //
+                    Text("Moff Cafe")
+                        .font(Font.system(size: 30, weight: .bold))
+                        .multilineTextAlignment(.center)
+                        .overlay {
+                            LinearGradient(
+                                colors: [.pink, .teal, .purple, .blue],
+                                startPoint: .bottomTrailing,
+                                endPoint: .topLeading
+                            )
+                            .mask(
+                                Text("Moff Cafe")
+                                    .font(Font.system(size: 30, weight: .bold))
+                                    .multilineTextAlignment(.center)
+                            )
+                        }
+
+                
+                }
+                // use it for the next screen...
+                /*.offset(
+                    x: zoomed ? 500 : 30,
+                    y: -30
+                )
+                
+                .animation(.default, value: zoomed)
+                
+                
+                GeometryReader { geometry in
+                    Image("gat")
+                    
+                        .clipShape(
+                            RoundedRectangle(cornerRadius: zoomed ? 40 : 500)
+                        )
+                        .overlay(
+                            Circle()
+                                .fill(
+                                    zoomed
+                                    ? Color.clear
+                                    : Color(white: 1, opacity: 0.4)
+                                )
+                                .scaleEffect(0.8)
+                        )
+                        .saturation(zoomed ? 1 : 0)
+                        .position(
+                            x: zoomed ? geometry.frame(in: .local).midX : 600,
+                            y: 50
+                        )
+                        .scaleEffect((zoomed ? 4 : 1) / 3)
+                        .shadow(radius: 10)
+                    
+                        .animation(.spring(), value: zoomed)
+                        .onTapGesture { zoomed.toggle() }
+                        
+                }
+                 */
+
+            
+            
+          
+          
+                VStack {
+                    
+                    HStack {
+                        Text("Register to make reservation")
+                            .font(.caption)
+                        .foregroundColor(.indigo)
+                    }
+            
+                    Button(action: {} ) {
+                        
+                                Text("Register")
+                            .foregroundColor(.white)
+                           
+                           
+                          
+                            }
+                    .padding()
+                    .background(Color(red: 0.8, green: 0.4, blue: 0.8))
+                    .clipShape(RoundedRectangle(cornerRadius :  30))
+                }
+
+        
+    }
+}
+}
+
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View { ContentView() }
+}
---
+++ Gato/GatoApp.swift
@@ -0,0 +1,16 @@
+//
+//  GatoApp.swift
+//  Gato
+//
+//  Created by Ai on 2022/10/21.
+//
+
+import SwiftUI
+
+@main
+struct App: SwiftUI.App {
+
+    var body: some Scene {
+    WindowGroup(content: ContentView.init)
+  }
+}
