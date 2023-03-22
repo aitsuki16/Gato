1543e35164de65c9b4701d06f1b91b2f88a4e338
Aida Igarashi <>
2023-03-22 17:21:22 +0900

    Changed to fixed portraitMode

---
+++ Gato/AppDelegate.swift
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.swift
+//  Gato
+//
+//  Created by Aida Igarashi on 2023/03/22.
+//
+
+import UIKit
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, supportedInterfaceOrientationsFor window: UIWindow?) -> UIInterfaceOrientationMask {
+        return .portrait
+    }
+}
--- Gato/GatoApp.swift
+++ Gato/GatoApp.swift
@@ -6,11 +6,13 @@
 //
 
 import SwiftUI
-
 @main
 struct App: SwiftUI.App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
 
     var body: some Scene {
     WindowGroup(content: ContentView.init)
   }
 }
+
+
